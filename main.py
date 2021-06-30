90773f8c6383aeb5139aa249a7c969e5e3fc31ae
can <mustafacangencgul@gmai.com>
2021-06-30 15:31:08 +0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-sns.set_theme()
 
 tips = sns.load_dataset("tips")
+
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
+
+plt.show()
+
+
